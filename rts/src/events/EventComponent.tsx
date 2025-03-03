d1bfec909751521b53cff902ea827dfad305ba16
mlu1 <mthandemeluleki@yahoo.com>
2025-03-04 06:56:17 +0800

    add events

--- rts/src/events/EventComponent.tsx
+++ rts/src/events/EventComponent.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 const EventComponent:React.FC = () =>{
     
-    const onChange=(e)=>{
-        console.log(e)
+    const onChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        console.log(e.target.value)
+    }
+
+    const onDragStart = (e:React.ChangeEvent<HTMLDivElement>)=>{
+        console.log('im being dragged')
     }
     return(
         <div>
-            <input onChange={(e)=>console.log(e)}/>
+            <input onChange={onChange}/>
+            <div draggable onDragStart={onDragStart}>Drag Me</div>
         </div>
     )
 }
