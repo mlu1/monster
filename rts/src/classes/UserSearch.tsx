075374a496d7d0ddcecc9baec93b4e08c4c5db0d
mlu1 <mthandemeluleki@yahoo.com>
2025-03-04 08:08:22 +0800

    class based implementation

---
+++ rts/src/classes/UserSearch.tsx
@@ -0,0 +1,46 @@
+import React ,{Component,useState} from 'react'
+
+interface User{
+    name:string;
+    age:number
+}
+
+interface UserSearchProps{
+    users:User[]
+}
+
+interface UserSearchState{
+    name:string;
+    user:User|undefined;
+}
+
+class UserSearch extends Component<UserSearchProps>{
+    state:UserSearchState={
+        name:'',
+        user:undefined
+    }
+
+    onClick(){
+        const foundUser = this.props.users.find((user)=>{
+            return user.name ===name
+        });
+        this.setState({user:foundUser})
+    }
+    render(){
+        const {name,user} = this.state
+        return(
+            <div>
+            <h1>Search User</h1>
+            <input value = {this.state.name} 
+            onChange={e=>this.setState(name:e.target.value)}/>
+            <button onClick={onClick}>Find User</button>
+            <div>
+                {this.state.user && this.state.user.name}
+                {user && user.age}
+            </div>
+        </div>
+        )
+    }
+}
+
+export default UserSearch
