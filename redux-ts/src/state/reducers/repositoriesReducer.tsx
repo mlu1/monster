99adc6671a945989ab002feb95af160290665b04
mlu1 <mthandemeluleki@yahoo.com>
2025-03-18 23:29:35 +0800

    add action types

--- redux-ts/src/state/reducers/repositoriesReducer.tsx
+++ redux-ts/src/state/reducers/repositoriesReducer.tsx
@@ -18,20 +18,33 @@ interface SearchRepositoriesErrorAction{
     payload:string
 }
 
+type Action  = 
+    | SearchRepositoriesAction 
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction 
+
+enum ActionType{
+    SEARCH_REPOSITORIES = 'search_repositories',
+    SEARCH_REPOSITORIES_SUCCESS='search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR  = 'search_repositories_error'
+}
+
+ActionType.SEARCH_REPOSITORIES
+ActionType.SEARCH_REPOSITORIES_SUCCESS
+ActionType.SEARCH_REPOSITORIES_ERROR
+
 
 const reducer = (
     state:RepositoriesState,
-    action:SearchRepositoriesAction 
-          | SearchRepositoriesSuccessAction
-          | SearchRepositoriesErrorAction ,
+    action:Action
     ):RepositoriesState=>{
     switch(action.type){
-        case 'search_repositories':
-        return {loading:true,error:null,data:[]}
-        case 'search_repositories_success':
-        return{ loading:false,error:null,data:action.payload}
-        case 'search_repositories_error':
-        return{loading:false,error:action.payload,data:[]}
+        case ActionType.SEARCH_REPOSITORIES:
+            return {loading:true,error:null,data:[]}
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+            return{ loading:false,error:null,data:action.payload}
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
+            return{loading:false,error:action.payload,data:[]}
         default:return state
     }
 };
