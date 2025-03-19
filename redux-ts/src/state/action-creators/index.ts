1ff93076b65462befe89b336428bcd6b06a6ac84
mlu1 <mthandemeluleki@yahoo.com>
2025-03-19 23:42:37 +0800

    slight correction on feature

--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -1,3 +1,10 @@
 import axios from 'axios';
 import {ActionTypes} from '../action-types';
 import {Action} from '../actions';
+
+
+const searchRepositories =(item:string)=>{
+    return async (dispatch:any) => {
+        
+    }
+}
