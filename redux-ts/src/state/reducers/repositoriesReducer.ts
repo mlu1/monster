d774b2c2e98bd33268e08ef9d69e8897d4b3d653
mlu1 <mthandemeluleki@yahoo.com>
2025-04-08 03:33:52 +0800

    026

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -4,13 +4,30 @@ interface RepositoriesState{
     data:string[]
 }
 
-const reducer = (state:RepositoriesState,action:any)=>{
+interface SearchRepositoriesAction{
+    type:'search_repositories';
+}
+
+
+interface SearchRepositoriesSuccessAction{
+    type:'search_repositories_success';
+    payloads:string[]
+}
+
+
+interface SearchRepositoriesErrorAction{
+    type:'search_repositories_error';
+    payload:string;
+}
+
+
+const reducer = (state:RepositoriesState,action:Action):RepositoriesState=>{
     switch (action.type) {
         case 'search_repositories':
             return {loading:true,error:null,data:[]}
         case 'search_repositories_success':
             return {loading:false,error:null,data:action.payload}    
-        case 'search_repositoreis_error':
+        case 'search_repositories_error':
             return {loading:false,error action.payload,data:[]}
         default:
             return state
